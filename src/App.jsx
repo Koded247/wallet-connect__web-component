1267d0ea67e798dbe52aa4f04d64c612dcfffd54
abidoye korede <korexcoded@gmail.com>
2025-03-15 11:10:54 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function App() {
         </div>
       ) : (
         <button className="connect-btn" onClick={() => setIsModalOpen(true)}>
-          Connect Wallet <p>  </p>
+          Connect Wallet 
         </button>
       )}
 
@@ -87,7 +87,7 @@ function App() {
             <h2>Choose a Wallet</h2>
             <p className="modal-subtext">Select your preferred wallet:</p>
 
-            <button className="close-btn" onClick={() => setIsModalOpen(false)}>
+            <button className="close-btn text-red-700" onClick={() => setIsModalOpen(false)}>
               x
             </button>
             
